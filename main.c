761bdb9cf7c675609ff24d99d65b551148a2e8d2
Oussama Oukrid <oukrid.oussama@gmail.com>
2023-05-24 19:25:03 -0700

    betty lt2

--- main.c
+++ main.c
@@ -8,26 +8,23 @@
  */
 int main(int argc, char **argv, char **envp)
 {
-    int fd = STDIN_FILENO;
+int fd = STDIN_FILENO;
+if (argc == 2)
+{
+fd = open(argv[1], O_RDONLY);
+if (fd == -1)
+{
+if (errno == EACCES)
+exit(126);
+if (errno == ENOENT)
+exit(127);
+return (EXIT_FAILURE);
+}
+}
+signal(SIGINT, hundle_ctrl);
+prompt(argv, envp);
+if (fd != STDIN_FILENO)
+close(fd);
 
-    if (argc == 2)
-    {
-        fd = open(argv[1], O_RDONLY);
-        if (fd == -1)
-        {
-            if (errno == EACCES)
-                exit(126);
-            if (errno == ENOENT)
-                exit(127);
-            return (EXIT_FAILURE);
-        }
-    }
-    signal(SIGINT, hundle_ctrl);
-
-    prompt(argv, envp);
-
-    if (fd != STDIN_FILENO)
-        close(fd);
-
-    return (0);
+return (0);
 }
